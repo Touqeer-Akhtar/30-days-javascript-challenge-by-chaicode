777171dede17ecaba28026f8e28f388ec3e2b548
Touqeer-Akhtar <TouqeerAkhtar@outlook.com>
2024-07-13 18:02:04 +0000

    day 1 complete

---
+++ day1.js
@@ -0,0 +1,34 @@
+// Task 1
+var num = 5
+console.log(num);
+
+// Task 2
+let str = "hello"
+console.log(str);
+
+// Task 3 
+const bool = true
+console.log(bool);
+
+// Task 4 
+let obj = {
+    name: "hitesh",
+    id: 123
+}
+
+let arr = [9, "hello", true, "what?"]
+
+console.log(typeof num, typeof str, typeof bool, typeof obj, typeof arr);
+
+
+// Task 5
+let val = "initial value"
+console.log(val);
+
+val = "new value"
+console.log(val);
+
+// Task 6
+const tryingConst = "hello"
+
+// tryingConst = "new"     // error cannot assign new val in const
